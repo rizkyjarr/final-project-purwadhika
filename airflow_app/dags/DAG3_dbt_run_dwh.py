1f235dc29ba265ce66dbba6ae42a91b190b25ca3
Rizky Fajar <rizkyjarr@gmail.com>
2025-02-10 15:46:33 +0700

    currently dbt debug successfully worked, now developing DAG to run bash operators

--- airflow_app/dags/DAG3_dbt_run_dwh.py
+++ airflow_app/dags/DAG3_dbt_run_dwh.py
@@ -1,29 +1,18 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from datetime import datetime, timedelta
+from datetime import datetime
 
 default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2025, 2, 10),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "airflow",
+    "start_date": datetime(2024, 2, 10),
+    "retries": 1,
 }
 
-with DAG('dbt_run_dag', default_args=default_args, schedule_interval=None) as dag:
+with DAG("dbt_run_staging", default_args=default_args, schedule_interval="@daily", catchup=False) as dag:
 
-    run_dbt_staging = BashOperator(
-        task_id='run_dbt_staging',
-        bash_command='dbt run --models staging --profiles-dir /opt/airflow/dbt --project-dir /opt/airflow/dbt_project',
+    dbt_run_staging = BashOperator(
+        task_id="dbt_run_staging_models",
+        bash_command="docker exec -i dbt-runner dbt run --project-dir /dbt --profiles-dir /dbt --select staging"
     )
 
-    run_dbt_facts = BashOperator(
-        task_id='run_dbt_facts',
-        bash_command='dbt run --models facts --profiles-dir /opt/airflow/dbt --project-dir /opt/airflow/dbt_project',
-    )
-
-    run_dbt_marts = BashOperator(
-        task_id='run_dbt_marts',
-        bash_command='dbt run --models marts --profiles-dir /opt/airflow/dbt --project-dir /opt/airflow/dbt_project',
-    )
-
-    run_dbt_staging >> run_dbt_facts >> run_dbt_marts
+    dbt_run_staging
